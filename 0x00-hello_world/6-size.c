1b4c2106ea03cdc51066b3f093ffff56f4846a7e
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-09 09:16:24 -0800

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - prints size of various types
+ * Return: Always 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
@@ -13,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n",sizeof(long int));
 	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int))
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
+		return (0);
 }	
