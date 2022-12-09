daa5b6d63c2db9658e545bc0eecac9b1c3e8d06a
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-09 08:13:59 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar,
- * but the outcome is a piece of art,
- * Return: O if exited properly
+ * main - prints the phrase with proper grammar
+ * 
+ * Return: O if exited properly, non-zero otherwise
  */
 int main(void)
 {
