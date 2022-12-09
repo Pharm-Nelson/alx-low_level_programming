0f2cdd80744b79435ff46ede6dd8397482e50af5
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-09 07:30:38 -0800

    multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main prints a given phrase
- * return: O if exited properly, non zero otherwise
+/**
+ * main given phrase
+ *
+ * return: O success
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-{
+}
