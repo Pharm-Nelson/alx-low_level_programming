6b4e51741646c42cad801f47c3cbd6fd677fb635
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-09 10:35:21 -0800

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - prints size of various types
- * Return: Always 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
