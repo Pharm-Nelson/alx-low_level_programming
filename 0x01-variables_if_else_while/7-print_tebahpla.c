19e725c5777206ea8b2d23a621d6f1d2ce5c8d41
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-12 19:14:42 -0800

    initial commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main -main block
+ * Description: prints all single digit number of base 10
+ * starting from 0, followed by a new line
+ * Return: 0
  */
-
 int main(void)
 {
-	char c;
+	char c = 'z';
 
-	c = 'z';
-	while
-		(c >='a') {
-			putchar(c);
-			c--;
-		}
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
 	putchar('\n');
 	return (0);
 }
+
