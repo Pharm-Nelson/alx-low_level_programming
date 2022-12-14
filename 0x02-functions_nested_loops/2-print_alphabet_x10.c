10a6846e63ecf17493db740f5fa25df7efdca923
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-14 09:15:06 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i = 0;
-	
+
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
@@ -18,4 +18,4 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 	i++;
 	}
-}	
+}
