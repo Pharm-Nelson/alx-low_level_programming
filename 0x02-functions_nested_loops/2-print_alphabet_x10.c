e98a48ab4d6437bd97ba5a8f8fa00a0c08787edb
Pharm-Nelson <nelsoneke3@gmail.com>
2022-12-14 02:23:47 -0800

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - alphabet
- * print_alphabet_x10 - function to print a - z ten times
+ * main -alphabet
+ * print_alphabet_x10 - print a - z ten times
  */
 
 void print_alphabet_x10(void)
